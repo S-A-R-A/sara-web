6f02133cc1bd4e895f4c12cab3d2b2447c9affa9
Ícaro Jerry <icaro.jerry@gmail.com>
2017-07-20 23:42:58 -0300

    Fixing auto generate slots

--- manager/signals.py
+++ manager/signals.py
@@ -2,14 +2,26 @@ from .models import Day, Room, TimeInterval, Slot
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+@receiver(post_save, sender=Day, dispatch_uid='day_indentifier')
+def create_slot_per_day(sender, instance, created, **kwargs):
+    if created:
+        for room in Room.objects.all():
+            for interval in TimeInterval.objects.all():
+                slot_new = Slot.objects.create(day = instance, room = room, time_interval = interval)
+                slot_new.save()
+
+@receiver(post_save, sender=TimeInterval, dispatch_uid='time_interval_indentifier')
+def create_slot_per_time_interval(sender, instance, created, **kwargs):
+    if created:
+        for room in Room.objects.all():
+            for day in Day.objects.all():
+                slot_new = Slot.objects.create(day = day, room = room, time_interval = instance)
+                slot_new.save()
 
 @receiver(post_save, sender=Room, dispatch_uid='room_indentifier')
-@receiver(post_save, sender=TimeInterval, dispatch_uid='time_interval_indentifier')
-@receiver(post_save, sender=Day, dispatch_uid='day_indentifier')
-def create_slot(sender, instance, created, **kwargs):
+def create_slot_per_room(sender, instance, created, **kwargs):
     if created:
         for day in Day.objects.all():
-            for room in Room.objects.all():
-                for interval in TimeInterval.objects.all():                    
-                    slot_new = Slot.objects.create(day = day, room = room, time_interval = interval)
-                    slot_new.save()
+            for interval in TimeInterval.objects.all():
+                slot_new = Slot.objects.create(day = day, room = instance, time_interval = interval)
+                slot_new.save()
