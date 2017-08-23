cc5bbc986ba587a246fec00fa0df6d4e1f7bd557
IcaroJerry <icarojerry@gmail.com>
2017-08-23 13:23:26 -0300

    Add period checkbox and some improvements

--- manager/urls.py
+++ manager/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.show_timetabling),
-    url(r'^timetabling/(?P<dayid>\d+)', views.show_timetabling),
+    url(r'^timetabling/(?P<dayid>\d+)/(?P<periodid>\d+)', views.show_timetabling),
 ]
--- manager/views.py
+++ manager/views.py
@@ -4,16 +4,20 @@ from .models import Room
 from .models import TimeInterval
 from .models import Slot
 from .models import Class
+from .models import Period
 
-def show_timetabling(request, dayid=0):
+def show_timetabling(request, dayid=0, periodid=0):
     days = Day.objects.all()
+    periods = []
 
     if int(dayid) == 0:
         day = Day.objects.first()
     else:
         day = Day.objects.get(id = int(dayid))
 
+    periods = Period.objects.all()
+
     table = dict(day = day, time_intervals = day.time_intervals.all(),
                  rooms = Room.objects.all(), slots = list(Slot.objects.filter(day = day)))
 
-    return render(request, 'manager/show_timetabling.html', {'days': days, 'table': table})
+    return render(request, 'manager/show_timetabling.html', {'days': days, 'periods': periods, 'periodid': int(periodid), 'table': table})
