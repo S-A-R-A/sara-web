e573f86e2e9176fb674f71df53cb5d9c5af2c875
Ícaro Jerry <icaro.jerry@gmail.com>
2017-08-23 21:01:47 -0300

    Adding dropdown to choose area

--- manager/urls.py
+++ manager/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.show_timetabling),
+    url(r'^timetabling/(?P<dayid>\d+)/(?P<periodid>\d+)/(?P<areaid>\d+)', views.show_timetabling),
     url(r'^timetabling/(?P<dayid>\d+)/(?P<periodid>\d+)', views.show_timetabling),
     url(r'^timetabling/(?P<dayid>\d+)', views.show_timetabling),
     url(r'^timetabling', views.show_timetabling),
