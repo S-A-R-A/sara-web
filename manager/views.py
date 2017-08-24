4f202f25cc679f3dcc52ce248fafcabb6f64b349
Ícaro Jerry <icaro.jerry@gmail.com>
2017-08-23 21:01:47 -0300

    Adding dropdown to choose area

--- manager/views.py
+++ manager/views.py
@@ -5,19 +5,20 @@ from .models import TimeInterval
 from .models import Slot
 from .models import Class
 from .models import Period
+from .models import Area
 
-def show_timetabling(request, dayid=0, periodid=0):
+def show_timetabling(request, dayid=0, periodid=0, areaid=0):
     days = Day.objects.all()
-    periods = []
+    areas = Area.objects.all()
+    periods = Period.objects.all()
 
     if int(dayid) == 0:
         day = Day.objects.first()
     else:
         day = Day.objects.get(id = int(dayid))
 
-    periods = Period.objects.all()
-
     table = dict(day = day, time_intervals = day.time_intervals.all(),
                  rooms = Room.objects.all(), slots = list(Slot.objects.filter(day = day)))
 
-    return render(request, 'manager/show_timetabling.html', {'days': days, 'periods': periods, 'periodid': int(periodid), 'table': table})
+    return render(request, 'manager/show_timetabling.html', {'days': days, 'areas': areas, 'areaid': int(areaid),
+                                                             'periods': periods, 'periodid': int(periodid), 'table': table})
