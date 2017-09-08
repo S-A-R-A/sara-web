7799186a198ad3e8c5a818c663196857ce8cf1f2
Ícaro Jerry <icaro.jerry@gmail.com>
2017-09-07 21:55:54 -0300

    Adding log to commands

--- manager/management/commands/fill_data.py
+++ manager/management/commands/fill_data.py
@@ -19,21 +19,25 @@ class Command(BaseCommand):
 
     def fill_class_assignment(self, data):
         for element in data:
+            saved_class = Class.objects.get(id = element["s_class"])
+            print("{0} slots to {1}: ".format(len(element["slots"]), saved_class))
             for slot in element["slots"]:
-                saved_class = Class.objects.get(id = element["s_class"])
                 saved_slot = Slot.objects.get(day = slot["day"], time_interval = slot["time_interval"], room = slot["room"])
                 if saved_slot and saved_class:
                     saved_slot.s_class = saved_class
                     saved_slot.save()
+                    print("    {0} <- {1}".format(saved_class, saved_slot))
 
     def fill_timetabling(self, data):
         for element in data:
+            saved_class = Class.objects.get(id = element["s_class"])
+            print("{0} schedules to {1}: ".format(len(element["schedules"]), saved_class))
             for schedule in element["schedules"]:
-                saved_class = Class.objects.get(id = element["s_class"])
                 if saved_class:
                     saved_schedule = Schedule.objects.get(day = schedule["day"], time_interval = schedule["time_interval"])
                     if saved_schedule:
                         saved_class.schedules.add(saved_schedule)
+                        print("    {0} <- {1}".format(saved_class, saved_schedule))
 
     def add_arguments(self, parser):
         parser.add_argument('type_request', type=str)
