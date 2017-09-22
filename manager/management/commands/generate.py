6a54ecd9de3aa4a9d521ee47fb6e6b469dac350f
IcaroJerry <icarojerry@gmail.com>
2017-09-22 11:08:53 -0300

    Update generate command (beta)

--- manager/management/commands/generate.py
+++ manager/management/commands/generate.py
@@ -2,7 +2,10 @@ from django.core.management.base import BaseCommand, CommandError
 from manager.models import Class
 from manager.models import Schedule
 from manager.models import Slot
+from manager.models import Room
 import json
+
+
 
 class Command(BaseCommand):
 
@@ -15,16 +18,41 @@ class Command(BaseCommand):
         with open(file_name, 'w') as file:
             file.write(str(data))
 
-    def prepare_allocation_file(self, file_name):
-        list_models=[]
-        for slot in Slot.objects.all():
-            dict_model = {
-                "code": slot.id,
+    def models_to_file(self, file_name):
+        models = {
+            "slots": [],
+            "classes": [],
+            "rooms": []
+        }
+
+        slots = Slot.get_slots_by_schedules(Schedule.get_used_slots())
+        classes = Class.get_scheduled_classes()
+        rooms = Room.objects.all()
+
+        for slot in slots:
+            slot_model = {
+                "id": slot.id,
                 "capacity": slot.room.capacity,
+                "room": slot.room.id,
+                "schedule": Schedule.objects.get(day = slot.day, time_interval = slot.time_interval).id,
                 "requirements": []
             }
-            list_models.append(dict_model)
-        self.generate_allocation_file(file_name, json.dumps(list_models, indent=4, sort_keys=True))
+            models["slots"].append(slot_model)
+
+        for s_class in classes:
+            class_model = {
+                "id": s_class.id,
+                "size": s_class.size
+            }
+            models["classes"].append(class_model)
+
+        for room in rooms:
+            room_model = {
+                "id": room.id
+            }
+            models["rooms"].append(room_model)
+        self.create_json_data(file_name, json.dumps(models, indent=4, sort_keys=False))
+
 
     def add_arguments(self, parser):
         parser.add_argument('type_request', type=str)
@@ -35,12 +63,12 @@ class Command(BaseCommand):
         file_name = options['file_name']
         print("processing request...")
 
-        if type_request == "empty":
-            for x in Slot.get_empty_slots() :
-                print(x)
-        elif type_request == "filled":
-           for x in Slot.get_filled_slots() :
-               print(x)
+        if type_request == "request":
+            file_name = "teste.json"
+            print("creating {0} file...".format(file_name))
+            self.models_to_file(file_name)
+            print("the file was created...")
+            print("request \"{0}\" completed successfully...".format(type_request))
 
         else:
             print("type request invalid!")
